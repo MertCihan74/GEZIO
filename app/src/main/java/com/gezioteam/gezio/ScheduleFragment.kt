30a41b1d23e9bc152104524a69ebc36e40e75e9c
Mert Cihan Bayır <mert.bayir@ogr.sakarya.edu.tr>
2023-12-05 17:41:25 +0300

    Schedule Page implementation

--- app/src/main/java/com/gezioteam/gezio/ScheduleFragment.kt
+++ app/src/main/java/com/gezioteam/gezio/ScheduleFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 
 class ScheduleFragment : Fragment() {
@@ -14,6 +15,7 @@ class ScheduleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_schedule, container, false)
     }
 

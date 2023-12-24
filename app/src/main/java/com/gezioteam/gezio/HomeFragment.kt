28ea6d8ddba17ae97837f1d328ee22209f6e19a6
Mert Cihan Bayır <mert.bayir@ogr.sakarya.edu.tr>
2023-12-24 22:38:12 +0300

    Homepage adjustments

--- app/src/main/java/com/gezioteam/gezio/HomeFragment.kt
+++ app/src/main/java/com/gezioteam/gezio/HomeFragment.kt
@@ -5,16 +5,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import java.lang.reflect.Array
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.Toast
 
 class HomeFragment : Fragment() {
 
-  override fun onCreateView(
+    private var isFavorite = false
+
+    override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
 
+        val favs = view.findViewById<CheckBox>(R.id.imageView15)
+        favs.setOnClickListener {
+            isFavorite = !isFavorite
+
+            val toastMessage = if (isFavorite) {
+                "Added to your favorite places"
+            } else {
+                "Removed from favorite places"
+            }
+
+            Toast.makeText(requireContext(), toastMessage, Toast.LENGTH_SHORT).show()
+        }
+        val favs1 = view.findViewById<CheckBox>(R.id.imageView6)
+        favs1.setOnClickListener {
+            isFavorite = !isFavorite
+
+            val toastMessage = if (isFavorite) {
+                "Added to your favorite places"
+            } else {
+                "Removed from favorite places"
+            }
+
+            Toast.makeText(requireContext(), toastMessage, Toast.LENGTH_SHORT).show()
+        }
+        val favs2 = view.findViewById<CheckBox>(R.id.imageView23)
+        favs2.setOnClickListener {
+            isFavorite = !isFavorite
+
+            val toastMessage = if (isFavorite) {
+                "Added to your favorite places"
+            } else {
+                "Removed from favorite places"
+            }
+
+            Toast.makeText(requireContext(), toastMessage, Toast.LENGTH_SHORT).show()
+        }
+        return view
+    }
 }
